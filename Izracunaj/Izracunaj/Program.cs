ffebfce2e23486bbeeacf9f422a404d975491d4a
Vicko Cagalj <vickocagalj@gmail.com>
2022-01-20 18:24:45 +0100

    Add project files.

---
+++ Izracunaj/Izracunaj/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Izracunaj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 3)
+                throw new ArgumentException(
+                    "Please enter command line arguments in the following form: firstNumber operator secondNumber");
+
+            string operand = args[1];
+            double firstNumber = 0.0, secondNumber = 0.0;
+
+            try
+            {
+                firstNumber = double.Parse(args[0]);
+                secondNumber = double.Parse(args[2]);
+            }
+            catch (FormatException  e)
+            {
+                Console.WriteLine($"Catching the exception -> {e}");
+            }
+            
+            switch (operand)
+            {
+                case "+":
+                    Console.WriteLine(firstNumber + secondNumber);
+                    break;
+                case "-":
+                    Console.WriteLine(firstNumber - secondNumber);
+                    break;
+                case "*":
+                    Console.WriteLine(firstNumber * secondNumber);
+                    break;
+                case "/":
+                    Console.WriteLine(firstNumber / secondNumber);
+                    break;
+            }
+        }
+    }
+}
---
+++ MojaPrvaKonzola/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class MojaPrvaKonzola
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Brojevi djeljivi s 3:");
+            for (int i = 1; i < 100; ++i)
+            {
+                if (i % 3 == 0)
+                    Console.WriteLine(i);
+            }
+            Console.WriteLine("GOTOVO! Pritisni bilo koju tipku.");
+            Console.ReadKey();
+        }
+    }
+}
